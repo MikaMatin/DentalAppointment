4b739500e3428f7a58256784b5dc6fe65d2c2d0d
MikaMatin <mrmatinazad@mail.lipscomb.edu>
2016-10-27 14:18:10 -0500

    touch draft. not complete yet.

---
+++ Time.java
@@ -0,0 +1,16 @@
+
+public class Time {
+	
+	private int hours;
+	private int minutes;
+	
+	public void display()
+	{
+		System.out.print(this.hours+ ":"+this.minutes);
+	}
+	public Time()
+	{
+		
+	}
+	
+}
